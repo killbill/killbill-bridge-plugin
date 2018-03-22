4c69db2676a51e8cd3b8d052250af208540225ec
Pierre-Alexandre Meyer <pierre@mouraf.org>
2018-03-22 10:23:04 -0700

    converter: fix NPE
    Seen in a test system:
    
    Caused by: java.lang.NullPointerException: null
        at org.killbill.billing.plugin.bridge.api.converter.PaymentTransactionInfoPluginListResultConverter.convertModelToApi(PaymentTransactionInfoPluginListResultConverter.java:29)
        at org.killbill.billing.plugin.bridge.api.converter.PaymentTransactionInfoPluginListResultConverter.convertModelToApi(PaymentTransactionInfoPluginListResultConverter.java:25)
        at org.killbill.billing.plugin.bridge.api.BridgePaymentPluginApi.internalGenericPaymentTransactionOperation(BridgePaymentPluginApi.java:416)
        at org.killbill.billing.plugin.bridge.api.BridgePaymentPluginApi.getPaymentInfo(BridgePaymentPluginApi.java:139)
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- src/main/java/org/killbill/billing/plugin/bridge/api/converter/PaymentTransactionInfoPluginListResultConverter.java
+++ src/main/java/org/killbill/billing/plugin/bridge/api/converter/PaymentTransactionInfoPluginListResultConverter.java
@@ -17,18 +17,24 @@
 
 package org.killbill.billing.plugin.bridge.api.converter;
 
-import org.killbill.billing.payment.plugin.api.PaymentTransactionInfoPlugin;
-
 import java.util.List;
 import java.util.stream.Collectors;
+
+import org.killbill.billing.payment.plugin.api.PaymentTransactionInfoPlugin;
+
+import com.google.common.collect.ImmutableList;
 
 public class PaymentTransactionInfoPluginListResultConverter implements ResultConverter<org.killbill.billing.client.model.Payment, List<PaymentTransactionInfoPlugin>> {
 
     @Override
     public List<PaymentTransactionInfoPlugin> convertModelToApi(final org.killbill.billing.client.model.Payment payment) {
-        return payment.getTransactions()
-                .stream()
-                .map(targetTransaction -> new PaymentTransactionInfoPluginResultConverter().convertModelToApi(targetTransaction))
-                .collect(Collectors.toList());
+        if (payment == null || payment.getTransactions() == null) {
+            return ImmutableList.<PaymentTransactionInfoPlugin>of();
+        } else {
+            return payment.getTransactions()
+                          .stream()
+                          .map(targetTransaction -> new PaymentTransactionInfoPluginResultConverter().convertModelToApi(targetTransaction))
+                          .collect(Collectors.toList());
+        }
     }
 }
--- src/main/java/org/killbill/billing/plugin/bridge/api/converter/PaymentTransactionInfoPluginResultConverter.java
+++ src/main/java/org/killbill/billing/plugin/bridge/api/converter/PaymentTransactionInfoPluginResultConverter.java
@@ -22,25 +22,24 @@ import org.killbill.billing.client.model.PaymentTransaction;
 import org.killbill.billing.payment.api.TransactionType;
 import org.killbill.billing.payment.plugin.api.PaymentTransactionInfoPlugin;
 import org.killbill.billing.plugin.api.payment.PluginPaymentTransactionInfoPlugin;
-import org.killbill.billing.plugin.bridge.api.BridgePaymentPluginApi;
 
 public class PaymentTransactionInfoPluginResultConverter implements ResultConverter<PaymentTransaction, PaymentTransactionInfoPlugin> {
 
     @Override
     public PaymentTransactionInfoPlugin convertModelToApi(final PaymentTransaction paymentTransaction) {
-        return new PluginPaymentTransactionInfoPlugin(paymentTransaction.getPaymentId(),
-                paymentTransaction.getTransactionId(),
-                TransactionType.valueOf(paymentTransaction.getTransactionType()),
-                paymentTransaction.getAmount(),
-                Currency.valueOf(paymentTransaction.getCurrency()),
-                ConverterHelper.toPluginStatus(paymentTransaction.getStatus()),
-                paymentTransaction.getGatewayErrorMsg(),
-                paymentTransaction.getGatewayErrorCode(),
-                paymentTransaction.getFirstPaymentReferenceId(),
-                paymentTransaction.getSecondPaymentReferenceId(),
-                paymentTransaction.getEffectiveDate(),
-                paymentTransaction.getEffectiveDate(),
-                ConverterHelper.convertToApiPluginProperties(paymentTransaction.getProperties()));
+        return paymentTransaction == null ? null : new PluginPaymentTransactionInfoPlugin(paymentTransaction.getPaymentId(),
+                                                                                          paymentTransaction.getTransactionId(),
+                                                                                          TransactionType.valueOf(paymentTransaction.getTransactionType()),
+                                                                                          paymentTransaction.getAmount(),
+                                                                                          Currency.valueOf(paymentTransaction.getCurrency()),
+                                                                                          ConverterHelper.toPluginStatus(paymentTransaction.getStatus()),
+                                                                                          paymentTransaction.getGatewayErrorMsg(),
+                                                                                          paymentTransaction.getGatewayErrorCode(),
+                                                                                          paymentTransaction.getFirstPaymentReferenceId(),
+                                                                                          paymentTransaction.getSecondPaymentReferenceId(),
+                                                                                          paymentTransaction.getEffectiveDate(),
+                                                                                          paymentTransaction.getEffectiveDate(),
+                                                                                          ConverterHelper.convertToApiPluginProperties(paymentTransaction.getProperties()));
 
     }
 }
